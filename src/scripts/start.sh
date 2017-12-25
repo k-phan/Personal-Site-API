32279b7803799ca60e8c16fc891a2877f69ec2a3
Khai Phan <kphan95@bu.edu>
2017-12-25 17:46:47 -0500

    Configure Continuous Integration and Setup Testing

---
+++ src/scripts/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /var/bin/api/
+npm install 
+
+pm2 describe api > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start /srv/api/bin/api
+else
+  pm2 restart api
+fi;
